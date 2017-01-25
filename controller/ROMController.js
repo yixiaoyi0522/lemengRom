15c9b512811b5cd11824e06c455ec954ab6d85ed
MengXJ <907564255@qq.com>
2017-01-25 09:15:02 +0800

    1.0

---
+++ controller/ROMController.js
@@ -0,0 +1,6 @@
+var app = angular.module('ROMApp',[]);
+app.controller('ROMCtrl',function($scope){
+	$scope.urltop = 'resources/head.html';
+	$scope.urltop_more = 'resources/head_more.html';
+	$scope.urlfooter = 'resources/footer.html';
+})
